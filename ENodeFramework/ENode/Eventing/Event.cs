19f92d37506a2ad0e8e47ac402da61d863f0d3d6
xuehua <txh_723@163.com>
2013-10-14 08:53:27 +0800

    sync enode framework code.

--- ENodeFramework/ENode/Eventing/Event.cs
+++ ENodeFramework/ENode/Eventing/Event.cs
@@ -1,15 +1,31 @@
 ﻿using System;
-using ENode.Messaging;
 
 namespace ENode.Eventing
 {
     /// <summary>Represents a base domain event.
     /// </summary>
     [Serializable]
-    public class Event : Message, IEvent
+    public class Event : IEvent
     {
-        /// <summary>Default constructor.
+        /// <summary>Parameterized constructor.
         /// </summary>
-        public Event() : base(Guid.NewGuid()) { }
+        /// <param name="sourceId"></param>
+        /// <exception cref="ArgumentNullException"></exception>
+        public Event(object sourceId)
+        {
+            Id = Guid.NewGuid();
+            if (sourceId == null)
+            {
+                throw new ArgumentNullException("sourceId");
+            }
+            SourceId = sourceId;
+        }
+
+        /// <summary>Represents the unique identifier for the event.
+        /// </summary>
+        public Guid Id { get; private set; }
+        /// <summary>Represents the source of the event, which means which aggregate raised this event.
+        /// </summary>
+        public object SourceId { get; private set; }
     }
 }
--- ENodeFramework/Extensions/ENode.Redis/ConfigurationExtensions.cs
+++ ENodeFramework/Extensions/ENode.Redis/ConfigurationExtensions.cs
@@ -1,4 +1,6 @@
 ﻿using ENode.Domain;
+using ENode.Eventing;
+using ENode.Infrastructure.Serializing;
 
 namespace ENode.Redis
 {
@@ -6,19 +8,42 @@ namespace ENode.Redis
     /// </summary>
     public static class ConfigurationExtensions
     {
-        /// <summary>Use Redis as the memory cache for the enode framework.
+        /// <summary>Use Redis to implement the memory cache for the enode framework.
         /// </summary>
         /// <returns></returns>
-        public static Configuration UseRedis(this Configuration configuration)
+        public static Configuration UseRedisMemoryCache(this Configuration configuration)
         {
-            return UseRedis(configuration, "127.0.0.1", 6379);
+            return UseRedisMemoryCache(configuration, "127.0.0.1", 6379);
         }
-        /// <summary>Use Redis as the memory cache for the enode framework.
+        /// <summary>Use Redis to implement the memory cache for the enode framework.
         /// </summary>
         /// <returns></returns>
-        public static Configuration UseRedis(this Configuration configuration, string host, int port)
+        public static Configuration UseRedisMemoryCache(this Configuration configuration, string host, int port)
         {
             configuration.SetDefault<IMemoryCache, RedisMemoryCache>(new RedisMemoryCache(host, port));
+            return configuration;
+        }
+        /// <summary>Use Redis to implement the eventstore for the enode framework.
+        /// </summary>
+        /// <returns></returns>
+        public static Configuration UseRedisEventStore(this Configuration configuration)
+        {
+            return UseRedisEventStore(configuration, "127.0.0.1", 6379);
+        }
+        /// <summary>Use Redis to implement the eventstore for the enode framework.
+        /// </summary>
+        /// <returns></returns>
+        public static Configuration UseRedisEventStore(this Configuration configuration, string host, int port)
+        {
+            configuration.SetDefault<IEventStore, RedisEventStore>(new RedisEventStore(host, port));
+            return configuration;
+        }
+        /// <summary>Use ServiceStack.Redis to implement the binary serializer for the enode framework.
+        /// </summary>
+        /// <returns></returns>
+        public static Configuration UseRedisBinarySerializer(this Configuration configuration)
+        {
+            configuration.SetDefault<IBinarySerializer, RedisBinarySerializer>(new RedisBinarySerializer());
             return configuration;
         }
     }
