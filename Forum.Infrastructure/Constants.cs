01de7581cff679fb62e7306f94486a8e1886c110
hewang.txh <hewang.txh@alibaba-inc.com>
2014-04-28 13:36:35 +0800

    Remove unused constants.

--- Forum.Infrastructure/Constants.cs
+++ Forum.Infrastructure/Constants.cs
@@ -2,12 +2,6 @@
 {
     public class Constants
     {
-        public const string EventTable = "Event";
-        public const string EventTableCommitIndexName = "PK_Registration";
-        public const string EventTableVersionIndexName = "PK_Registration";
-        public const string EventPublishInfoTable = "EventPublishInfo";
-        public const string EventHandleInfoTable = "EventHandleInfo";
-
         public const string RegistrationTable = "Registration";
         public const string RegistrationTablePrimaryKeyName = "PK_Registration";
         public const string AccountTable = "Account";
