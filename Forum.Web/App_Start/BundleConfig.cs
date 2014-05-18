8e5077bf151cd35782adb0259dc44be7c3f16b5d
tangxuehua <txh_723@163.com>
2014-05-18 22:25:29 +0800

    alter ui.

--- Forum.Web/App_Start/BundleConfig.cs
+++ Forum.Web/App_Start/BundleConfig.cs
@@ -1,5 +1,4 @@
-﻿using System.Web;
-using System.Web.Optimization;
+﻿using System.Web.Optimization;
 
 namespace Forum.Web
 {
@@ -9,37 +8,14 @@ namespace Forum.Web
         {
             bundles.IgnoreList.Clear();
             bundles.Add(new ScriptBundle("~/content/jquery").Include(
-                        "~/Content/jquery/jquery-{version}.min.js"));
-
-            bundles.Add(new ScriptBundle("~/content/jqueryui").Include(
-                        "~/Content/jquery/jquery-ui-{version}.min.js"));
-
-            bundles.Add(new ScriptBundle("~/content/jqueryval").Include(
-                        "~/Content/jquery/jquery.unobtrusive-ajax.min.js",
-                        "~/Content/jquery/jquery.validate.min.js",
-                        "~/Content/jquery/jquery.validate.unobtrusive.min.js"));
-
+                "~/Content/jquery/jquery-1.10.2.min.js"));
             bundles.Add(new ScriptBundle("~/content/bootstrap/js").Include(
-                        "~/Content/bootstrap/js/bootstrap.min.js"));
-
+                "~/Content/bootstrap/js/bootstrap.min.js"));
             bundles.Add(new StyleBundle("~/content/bootstrap/css").Include(
-                        "~/Content/bootstrap/css/bootstrap.min.css"));
-
-            bundles.Add(new StyleBundle("~/content/css").Include("~/Content/site.css"));
-
-            bundles.Add(new StyleBundle("~/content/themes/base/css").Include(
-                        "~/Content/themes/base/jquery.ui.core.css",
-                        "~/Content/themes/base/jquery.ui.resizable.css",
-                        "~/Content/themes/base/jquery.ui.selectable.css",
-                        "~/Content/themes/base/jquery.ui.accordion.css",
-                        "~/Content/themes/base/jquery.ui.autocomplete.css",
-                        "~/Content/themes/base/jquery.ui.button.css",
-                        "~/Content/themes/base/jquery.ui.dialog.css",
-                        "~/Content/themes/base/jquery.ui.slider.css",
-                        "~/Content/themes/base/jquery.ui.tabs.css",
-                        "~/Content/themes/base/jquery.ui.datepicker.css",
-                        "~/Content/themes/base/jquery.ui.progressbar.css",
-                        "~/Content/themes/base/jquery.ui.theme.css"));
+                "~/Content/bootstrap/css/bootstrap.min.css",
+                "~/Content/bootstrap/css/bootstrap-theme.min.css"));
+            bundles.Add(new StyleBundle("~/content/css").Include(
+                "~/Content/site.css"));
         }
     }
 }
---
+++ Forum.Web/Controllers/PostController.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using ENode.Commanding;
+using Forum.QueryServices;
+using Forum.QueryServices.DTOs;
+
+namespace Forum.Web.Controllers
+{
+    public class PostController : Controller
+    {
+        private readonly ICommandService _commandService;
+        private readonly IPostQueryService _postQueryService;
+
+        public PostController(ICommandService commandService, IPostQueryService postQueryService)
+        {
+            _commandService = commandService;
+            _postQueryService = postQueryService;
+        }
+
+        public ActionResult Index(string sectionId, int pageIndex)
+        {
+            return View(_postQueryService.QueryPosts(new PostQueryOption(pageIndex) { SectionId = sectionId }));
+        }
+        public ActionResult Detail(string id)
+        {
+            return View(_postQueryService.QueryPost(id));
+        }
+    }
+}
