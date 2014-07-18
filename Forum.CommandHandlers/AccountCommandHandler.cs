fa86051b775c855c1cbcbfd34f4ce7f923233a8f
hewang.txh <hewang.txh@alibaba-inc.com>
2014-07-18 16:34:46 +0800

    升级Enode的版本，重构账号注册的账号名称重复的设计。

--- Forum.CommandHandlers/AccountCommandHandler.cs
+++ Forum.CommandHandlers/AccountCommandHandler.cs
@@ -2,7 +2,6 @@
 using ENode.Commanding;
 using ENode.Infrastructure;
 using Forum.Commands.Accounts;
-using Forum.Domain;
 using Forum.Domain.Accounts;
 
 namespace Forum.CommandHandlers
@@ -12,23 +11,19 @@ namespace Forum.CommandHandlers
         ICommandHandler<RegisterNewAccountCommand>
     {
         private readonly ILockService _lockService;
-        private readonly AggregateRootFactory _factory;
-        private readonly RegisterAccountIndexService _registerAccountIndexService;
+        private readonly RegisterAccountService _registerAccountService;
 
-        public AccountCommandHandler(ILockService lockService, AggregateRootFactory factory, RegisterAccountIndexService registerAccountIndexService)
+        public AccountCommandHandler(ILockService lockService, RegisterAccountService registerAccountService)
         {
-            _factory = factory;
             _lockService = lockService;
-            _registerAccountIndexService = registerAccountIndexService;
+            _registerAccountService = registerAccountService;
         }
 
         public void Handle(ICommandContext context, RegisterNewAccountCommand command)
         {
             _lockService.ExecuteInLock(typeof(Account).Name, () =>
             {
-                var account = _factory.CreateAccount(command.Name, command.Password);
-                _registerAccountIndexService.RegisterAccountIndex(command.Id, account.Id, command.Name);
-                context.Add(account);
+                context.Add(_registerAccountService.RegisterNewAccount(command.Id, command.Name, command.Password));
             });
         }
     }
---
+++ Forum.Domain/Accounts/RegisterAccountService.cs
@@ -0,0 +1,49 @@
+﻿using ECommon.Components;
+using Forum.Infrastructure;
+
+namespace Forum.Domain.Accounts
+{
+    /// <summary>提供账号注册的领域服务，封装账号注册的业务规则，比如账号唯一性检查
+    /// </summary>
+    [Component(LifeStyle.Singleton)]
+    public class RegisterAccountService
+    {
+        private readonly IIdentityGenerator _identityGenerator;
+        private readonly IAccountIndexStore _accountIndexStore;
+        private readonly AggregateRootFactory _factory;
+
+        public RegisterAccountService(IIdentityGenerator identityGenerator, AggregateRootFactory factory, IAccountIndexStore accountIndexStore)
+        {
+            _identityGenerator = identityGenerator;
+            _factory = factory;
+            _accountIndexStore = accountIndexStore;
+        }
+
+        /// <summary>注册新账号
+        /// </summary>
+        /// <param name="accountIndexId"></param>
+        /// <param name="accountName"></param>
+        /// <param name="accountPassword"></param>
+        /// <returns></returns>
+        public Account RegisterNewAccount(string accountIndexId, string accountName, string accountPassword)
+        {
+            //首先创建一个新账号
+            var account = _factory.CreateAccount(accountName, accountPassword);
+
+            //先判断该账号是否存在
+            var accountIndex = _accountIndexStore.FindByAccountName(account.Name);
+            if (accountIndex == null)
+            {
+                //如果不存在，则添加到账号索引
+                _accountIndexStore.Add(new AccountIndex(accountIndexId, account.Id, account.Name));
+            }
+            else if (accountIndex.IndexId != accountIndexId)
+            {
+                //如果存在但和当前的索引ID不同，则认为是账号有重复
+                throw new DuplicateAccountException(accountName);
+            }
+
+            return account;
+        }
+    }
+}
