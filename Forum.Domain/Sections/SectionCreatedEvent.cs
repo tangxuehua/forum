ace08581a77035bf78671bee39f342dc81c42c01
郑志强 <4062499@qq.com>
2016-03-27 20:29:39 +0800

    Description  属性 改为 private set

--- Forum.Domain/Sections/SectionCreatedEvent.cs
+++ Forum.Domain/Sections/SectionCreatedEvent.cs
@@ -5,7 +5,7 @@ namespace Forum.Domain.Sections
     public class SectionCreatedEvent : DomainEvent<string>
     {
         public string Name { get; private set; }
-        public string Description { get; set; }
+        public string Description { get; private set; }
         private SectionCreatedEvent() { }
         public SectionCreatedEvent(string name, string description)
         {
